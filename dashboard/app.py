762c11b40bf3956020d30df36552c76505c419ad
Timothy Liu <tlkh.xms@gmail.com>
2019-03-28 07:18:37 +0800

    Added more info into dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -43,17 +43,27 @@ def end_server():
 
     return render_template("index.html")
 
-
 @app.route('/query', methods=["GET"])
 def get_server_data():
     try:
         container = dctl.get_cnt()
 
-        #print("\n",container.attrs,"\n")
+        #print("\n", container.attrs, "\n")
 
         port_binding = container.attrs["HostConfig"]["PortBindings"]["8888/tcp"][0]["HostPort"]
         vol_mount = container.attrs["HostConfig"]["Binds"]
+        runtime = container.attrs["HostConfig"]["Runtime"]
+        image = container.attrs["Config"]["Image"]
         state = container.attrs["State"]["Status"]
+
+        env = container.attrs["Config"]["Env"]
+        cuda_version = "unknown"
+        nccl_version = "unknown"
+        for item in env:
+            if "CUDA_VERSION" in item:
+                cuda_version = item.replace("_VERSION","")
+            elif "NCCL_VERSION" in item:
+                nccl_version = item.replace("_VERSION","")
 
         logs = str(container.logs())
 
@@ -62,16 +72,23 @@ def get_server_data():
         print("[INFO  ] Volume mount:", vol_mount)
 
         response = {"state": state,
+                    "cuda": cuda_version,
+                    "nccl": nccl_version,
+                    "image": image,
+                    "runtime": runtime,
                     "port": port_binding,
                     "vol": vol_mount,
                     "logs": logs}
 
     except Exception as e:
-        response = {"state": "Not running",
-                    "port": "Not running",
-                    "vol": "Not running",
-                    "logs": "No logs", 
-                    "error": str(e)}
+        response = {"state": "not running",
+                    "cuda": "unknown",
+                    "nccl": "unknown",
+                    "image": "none",
+                    "runtime": "none",
+                    "port": "none",
+                    "vol": "none",
+                    "logs": "No logs: "+str(e)}
 
     return flask.jsonify(response)
 
--- dashboard/static/js/app.js
+++ dashboard/static/js/app.js
@@ -7,6 +7,13 @@ var display_port = document.getElementById("display_port");
 var display_vol = document.getElementById("display_vol");
 var display_state = document.getElementById("display_state");
 var display_output = document.getElementById("display_output");
+var display_cmd = document.getElementById("display_cmd");
+
+var display_runtime = document.getElementById("display_runtime");
+var display_image = document.getElementById("display_image");
+
+var display_cuda = document.getElementById("display_cuda");
+var display_nccl = document.getElementById("display_nccl");
 
 var open_nb = document.getElementById("open_nb");
 var open_lab = document.getElementById("open_lab");
@@ -28,18 +35,18 @@ document.getElementById("startContainer").onclick = function () {
     console.log("Starting container");
     var path = "/start";
     var port_mount = inputport1.value;
-    if (port_mount=="") {
-        port_mount="8888";
+    if (port_mount == "") {
+        port_mount = "8888";
     }
-    var tag_mount = inputtag1.value;
-    if (tag_mount=="") {
-        tag_mount="latest";
+    var tag_version = inputtag1.value;
+    if (tag_version == "") {
+        tag_version = "latest";
     }
     var volume_mount = inputvolume1.value;
-    if (volume_mount=="") {
+    if (volume_mount == "") {
         alert("Volume mount cannot be empty");
     } else {
-        path = path + "?port=" + port_mount + "&vol=" + volume_mount + "&tag=" + tag_mount;
+        path = path + "?port=" + port_mount + "&vol=" + volume_mount + "&tag=" + tag_version;
         console.log(path);
         post(path);
     }
@@ -60,36 +67,57 @@ function update_display() {
     Http.onreadystatechange = (e) => {
         //console.log(Http.responseText)
         var response = JSON.parse(Http.responseText);
-        display_port.innerHTML = "<code>" + response["port"] + "</code>";
+        display_port.innerHTML = response["port"];
         port = response["port"];
         open_nb.href = "http://localhost:" + port + "/tree"
         open_lab.href = "http://localhost:" + port + "/lab"
 
         var volume_mount = response["vol"].toString();
-        display_vol.innerHTML = "<code>" + volume_mount.split(":")[0] + "</code>";
+        display_vol.innerHTML =  volume_mount.split(":")[0];
 
-        var logs = response["logs"].toString().replace(/'/g,"").replace(/\\n/g,"<br>");
-        display_output.innerHTML = "<pre><code>"+logs.substring(logs.length - 2000, logs.length)+"</code></pre>";
+        var logs = response["logs"].toString().replace(/'/g, "").replace(/\\n/g, "<br>");
+        display_output.innerHTML = "<pre><code>" + logs.substring(logs.length - 4000, logs.length) + "</code></pre>";
 
-        var state = response["state"]
+        var state = response["state"];
         if (state == "running") {
             display_state.classList.remove("badge-danger");
-            display_state.classList.remove("badge-warning");
             display_state.classList.add("badge-success");
         } else {
             if (state == "Not running") {
                 display_state.classList.remove("badge-success");
-                display_state.classList.remove("badge-warning");
                 display_state.classList.add("badge-danger");
             }
             else {
                 display_state.classList.remove("badge-success");
-                display_state.classList.remove("badge-danger");
-                display_state.classList.add("badge-warning");
+                display_state.classList.add("badge-danger");
             }
         }
         display_state.innerHTML = response["state"];
 
+        var runtime = response["runtime"];
+
+        if (runtime == "nvidia") {
+            display_runtime.classList.remove("badge-danger");
+            display_runtime.classList.add("badge-success");
+        } else {
+            display_runtime.classList.remove("badge-success");
+            display_runtime.classList.add("badge-danger");
+        }
+
+        display_runtime.innerHTML = runtime;
+
+        var image = response["image"];
+        display_image.innerHTML = image;
+
+        if (volume_mount=="none") {
+            display_cmd.innerHTML = "No container running";
+        } else {
+            display_cmd.innerHTML = "<code>nvidia-docker run --rm -p " + port + ":8888 -v " + volume_mount.split(":")[0] + ":/home/jovyan " + image + "</code>";
+        }
+
+        display_cuda.innerHTML = response["cuda"];
+        display_nccl.innerHTML = response["nccl"];
+        
     }
 }
 
