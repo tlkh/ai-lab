2412a8b3a4c612b95249ead1d5a7922e52a82823
Timothy Liu <tlkh.xms@gmail.com>
2019-06-07 17:50:10 +0800

    Updated build script

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-docker build . -f base.Dockerfile -t nvaitc/ai-lab:0.8-base
-docker build . -f full.Dockerfile -t nvaitc/ai-lab:0.8
-docker build . -f vnc.Dockerfile -t nvaitc/ai-lab:0.8-vnc
+TAG=0.8 
+
+echo -e "\nBuilding images\n"
+
+docker build . -f base.Dockerfile -t nvaitc/ai-lab:$TAG-base
+docker build . -f full.Dockerfile -t nvaitc/ai-lab:$TAG
+docker build . -f vnc.Dockerfile -t nvaitc/ai-lab:$TAG-vnc
+
+echo -e "\nPushing images\n"
+
+docker push nvaitc/ai-lab:$TAG-base
+docker push nvaitc/ai-lab:$TAG
+docker push nvaitc/ai-lab:$TAG-vnc
