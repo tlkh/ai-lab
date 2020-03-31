8ce5f7cf15439441a3777b57aecac69e4391932e
Timothy Liu <tlkh.xms@gmail.com>
2020-03-31 13:02:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,12 +12,6 @@ docker build . -f tf2.Dockerfile -t nvaitc/ai-lab:$TAG-tf2
 docker build . -f full.Dockerfile -t nvaitc/ai-lab:$TAG
 docker build . -f vnc.Dockerfile -t nvaitc/ai-lab:$TAG-vnc
 
-docker build . -f base.Dockerfile -t nvcr.io/nvidian/sae/ai-lab:$TAG-base
-#docker build . -f tf.Dockerfile -t nvcr.io/nvidian/sae/ai-lab:$TAG-tf
-docker build . -f tf2.Dockerfile -t nvcr.io/nvidian/sae/ai-lab:$TAG-tf2
-docker build . -f full.Dockerfile -t nvcr.io/nvidian/sae/ai-lab:$TAG
-docker build . -f vnc.Dockerfile -t nvcr.io/nvidian/sae/ai-lab:$TAG-vnc
-
 echo -e "\nPushing images\n"
 
 docker push nvaitc/ai-lab:$TAG-base
